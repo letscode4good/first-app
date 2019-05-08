c5c11e769b43907e94624ab31b416c6c9b361603
letscode4good <letscode4good@gmail.com>
2019-05-08 14:10:50 +0530

    adding db

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
+var bodyParser = require('body-parser')
+
+        app.use(bodyParser.urlencoded({ extended: false }))
+        app.use(bodyParser.json())
+
+        app.post('/login', function(req, res){
+            // if username = admin, password = admin
+            if(req.body.username == "admin" && req.body.password == "admin")
+                res.json({'message' : 'success'});
+            else
+                res.json({'message': 'failure'});
+        })
 
 var mongoose = require('mongoose');
 
