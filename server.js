a51319cac135b40eb7d26f21a9facc3b4ed02ba6
letscode4good <49649447+letscode4good@users.noreply.github.com>
2019-05-08 15:32:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/register', function(req, res){
 })
 
 app.post('/gettodo', function(req, res){
-    User.find({username : req.username}, function(err, userObj){
+    User.find({username : req.body.username}, function(err, userObj){
         res.json(userObj);
     })
 })
