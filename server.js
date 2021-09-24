4ea430b36a2c698027a09cb29c923e4a9f6afad8
rajat karandikar <letscode4good@gmail.com>
2021-09-24 22:24:23 +0530

    look for static jquery and css

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
-app.use(express.static(''))
+app.use(express.static('/'))
 
 var bodyParser = require('body-parser')
 
