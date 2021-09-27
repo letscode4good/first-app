7756112b223e77b1445f057d985893d024723dc8
rajat karandikar <letscode4good@gmail.com>
2021-09-27 12:15:15 +0530

    try api fix

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: true
+  }))
 
 var bodyParser = require('body-parser')
         app.use(bodyParser.urlencoded({ extended: true }))
