9b4e9a0d615df7e024350d4c40575184328013bf
rajat karandikar <letscode4good@gmail.com>
2021-12-18 23:18:15 +0530

    try connect to google cloud

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ const {Storage} = require('@google-cloud/storage');
 const storage = new Storage(
     {
         projectId: process.env.GOOGLE_CLOUD_PROJECT,
-        keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+        keyFilename: process.env.GOOGLE_CREDENTIALS,
       }
 );
 
