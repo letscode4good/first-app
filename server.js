911a0897a3449a492fd0e7b7dbdd0430f5d37718
rajat karandikar <letscode4good@gmail.com>
2022-06-09 19:37:01 +0530

    retry

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 80
-app.use(express.static(root))
+app.use(express.static('public'))
 app.use(express.urlencoded({
     extended: true
   }))
