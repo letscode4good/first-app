b0a7e94858d62102089635db795ede6f9137e537
rajat karandikar <letscode4good@gmail.com>
2022-06-09 20:14:30 +0530

    try caching

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 80
-app.use(express.static('public'))
+app.use(express.static('public',{ maxAge:86400000}))
 app.use(express.urlencoded({
     extended: true
   }))
